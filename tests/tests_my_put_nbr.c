3ff2b55e058794df9b8e3654c10d1f4657c936a8
pierre <pierre-alexandre.cochet@epitech.eu>
2021-10-06 17:22:47 +0200

    modif

---
+++ tests/tests_my_put_nbr.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** Day3_2021
+** File description:
+** tests_my_put_nbr
+*/
+
+int my_put_nbr(int nb);
+
+int main()
+{
+    my_isneg(0);
+    my_isneg(42);
+    my_isneg(-2147483647);
+    return(0);
+}
